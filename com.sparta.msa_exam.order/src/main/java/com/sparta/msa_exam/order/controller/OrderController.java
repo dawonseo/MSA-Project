c70288e0caa4ebf9412c5ffe4fb82768abeed76d
dawonseo <oloielo@naver.com>
2024-08-12 02:34:19 +0900

    주문 조회 API 캐싱 기능 추가

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/controller/OrderController.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.sparta.msa_exam.order.dto.OrderRequestDto;
 import com.sparta.msa_exam.order.dto.OrderResponseDto;
 import com.sparta.msa_exam.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,6 +28,7 @@ public class OrderController {
     }
 
     // 주문 단건 조회 API
+    @Cacheable(cacheNames = "orderCache", key = "args[0]")
     @GetMapping("/order/{orderId}")
     public OrderResponseDto getOneOrder(@PathVariable Long orderId) {
         return orderService.getOneOrder(orderId);
--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/dto/OrderResponseDto.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/dto/OrderResponseDto.java
@@ -1,14 +1,16 @@
 package com.sparta.msa_exam.order.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter @Setter
 @Builder
-public class OrderResponseDto {
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponseDto implements Serializable {
     private Long order_id;
     private List<Long> product_ids;
 }
